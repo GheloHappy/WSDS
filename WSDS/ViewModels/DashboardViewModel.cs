0f2dbe691a68d830cb2d5a7a6089c5f83bdcbe11
unknown <ghelonicomaligaya111@gmail.com>
2022-07-07 16:25:23 +0800

    revise quotation form

---
+++ WSDS/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weambard_Sales_Doc_Sys_WPF.Core;
+
+namespace WSDS.ViewModels
+{
+    internal class DashboardViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ClientsViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        //public ClientsViewModel ClientsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public DashboardViewModel()
+        {
+            HomeVM = new();
+            //ClientsVM = new();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            //ClientsViewCommand = new RelayCommand(o =>
+            //{
+            //    CurrentView = ClientsVM;
+            //});
+        }
+    }
+}
